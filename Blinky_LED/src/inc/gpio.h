4b239b7b92e7f58886b6dea48d7602caf65fbbe1
ahmadmorad39 <ahmadmorad39@gmail.com>
2025-02-18 10:44:05 +0200

    Rearrangement files of blinky LED

--- Blinky_LED/src/inc/gpio.h
+++ Blinky_LED/src/inc/gpio.h
@@ -1,113 +1,81 @@
 /*
  * gpio.h
  *
- *  Created on: Nov 19, 2024
- *  Author    : ahmad
+ * @author  Ahmad Murad
+ * @date    Feb 18, 2025
  *
- * @brief   Some examples on how to use STM32 GPIOs
+ * @brief   some examples on how to use STM32 GPIOs
  *
  * @note    Example: calculate the bit band address of the green LED
- *                   connected to PG13
+ *			        connected to PG13
  *
- *          General equation:
- *          bit_word_addr = bit_band_base + (byte_offset * 32) + (bit_number * 4)
+ *			General equation:
+ *			bit_word_addr = bit_band_base + (byte_offset * 32) + (bit_number * 4)
  *
- *          peripheral_base_address = 0x40000000 (Fixed for all ARM Cortex-M4)
- *          bit_band_base           = 0x42000000 (Fixed for all ARM Cortex-M4)
- *          port_G_address          = 0x40021800 (From STM32F4 datasheet)
- *          register_ODR_offset     = 0x14       (From STM32F4 reference manual)
+ *			peripheral_base_address = 0x40000000 (Fixed for all ARM Cortex-M4)
+ *			bit_band_base           = 0x42000000 (Fixed for all ARM Cortex-M4)
+ *			port_G_address          = 0x40021800 (From STM32F4 datasheet)
+ *			register_ODR_offset     = 0x14       (From STM32F4 datasheet)
  *
- *          byte_offset = port_G_address + register_ODR_offset - peripheral_base_address
- *                      = 0x40021800 + 0x14 - 0x40000000
- *                      = 0x00021814
+ *			byte_offset = port_G_address + register_ODR_offset - peripheral_base_address
+ *						= 0x40021800 + 0x14 - 0x40000000
+ *						= 0x00021814
  *
- *          bit_number  = 13 (Green LED connected to PG13)
+ *			bit_number = 13 (Green LED connected to PG13)
  *
- *          bit_word_addr = 0x42000000 + (0x00021814 * 32) + (13 * 4)
- *                        = 0x42000000 + 0x00430280 + 0x34
- *                        = 0x424302B4
+ *			bit_word_addr = 0x42000000 + (0x00021814 * 32) + (13 * 4)
+ *						  = 0x424302B4
+ *
  */
 
 #ifndef INC_GPIO_H_
 #define INC_GPIO_H_
 
-/* C++ detection */
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/* Includes */
 #include "stm32f4xx.h"
+#include "stm32f4xx_it.h"
+#include "stm32f429i_discovery.h"
 
 /**
- * @brief   On-board LEDs
+ * @brief  Define base address of peripheral bit-band
+ *
+ */
+#define BITBAND_PERI_BASE  0x40000000
+
+/**
+ * @brief  Define base address of peripheral alias band
+ */
+
+#define ALIAS_PERI_BASE   0x42000000
+
+/**
+ * @brief  Define address of GPIOG
+ */
+#define GPIOG_ODR         0x40021800
+
+/**
+ * @brief  Convert PERI address to alias region
+ */
+#define BITBAND_PERI(reg_address, bit_number) ((ALIAS_PERI_BASE + \
+		(reg_address - BITBAND_PERI_BASE) * 32 + (bit_number * 4)))
+
+#define GREEN_LED_BITBAND *((volatile uint32_t *) BITBAND_PERI(GPIOG_ODR,13))
+
+
+
+/**
+ * @brief  On-board LEDs
  */
 typedef enum
 {
-  EVAL_GREEN_LED,
-  EVAL_GREEN_LED_BITBAND,
-  EVAL_RED_LED,
-  EVAL_ALL_LEDs
+	EVAL_GREEN_LED,
+	EVAL_GREEN_LED_BITBAND,
+	EVAL_RED_LED,
+	EVAL_ALL_LEDs
 } Led_Type;
 
-/**
- * @brief   Define base address of peripheral bit-band
- */
-#define BITBAND_PERI_BASE 0x40000000
-
-/**
- * @brief   Define base address of peripheral alias band
- */
-#define ALIAS_PERI_BASE 0x42000000
-
-/**
- * @brief   Define address of GPIOG
- */
-#define GPIOG_ODR 0x40021814
-
-/**
- * @brief   Convert PERI address to alias region
- */
-#define BITBAND_PERI(reg_address, bit_number) ((ALIAS_PERI_BASE + \
-                (reg_address - BITBAND_PERI_BASE) * 32 + (bit_number * 4)))
-
-/**
- * @brief   Green LED connected to GPIOG bit 13
- */
-#define GREEN_LED_BITBAND *((volatile uint32_t *)(BITBAND_PERI(GPIOG_ODR, 13)))
-
-/**
- * @brief   On-board LEDs initialization function
- * @note    EVAL_GREEN_LED -> PG13
- * @param
- * @retval
- */
-void GPIO_Init_LED(Led_Type led);
-
-/**
- * @brief   Turns on-board LED on
- * @note    EVAL_GREEN_LED -> PG13
- * @param   Led_Type led
- * @retval
- */
 void GPIO_TurnON_LED(Led_Type led);
-
-/**
- * @brief   Turns on-board LED off
- * @note    EVAL_GREEN_LED -> PG13
- * @param   Led_Type led
- * @retval
- */
+void GPIO_Init_LED(Led_Type led);
 void GPIO_TurnOFF_LED(Led_Type led);
-
-/**
- * @}
- */
-
-/* C++ detection */
-#ifdef __cplusplus
-}
-#endif
 
 
 #endif /* INC_GPIO_H_ */
--- Blinky_LED/src/src/gpio.c
+++ Blinky_LED/src/src/gpio.c
@@ -1,109 +1,84 @@
 /*
- * gpio.c
+ * @file    gpio.c
+ * @author  Ahmad Murad
+ * @date    Feb 18, 2025
  *
- *  Created on: Nov 19, 2024
- *  Author    : ahmad
+ *@brief   some examples on how to use STM32 GPIOs
  *
- *  @brief   Some examples on how to use STM32 GPIOs
  */
+
+
 
 
 /* Includes */
 #include "inc/gpio.h"
 
 /**
- * @brief   On-board LEDs initialization function
+ * @brief   on-board LEDs initialization function
  * @note    EVAL_GREEN_LED -> PG13
- * @param
- * @retval
  */
 void GPIO_Init_LED(Led_Type led)
 {
-  /* Check LED type, please */
-  switch (led)
-  {
-    case EVAL_GREEN_LED:
+	/* Check LED type, please */
+	switch (led)
+	{
+		case EVAL_GREEN_LED:
 
-      /* Enable port G clock */
-      RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;
+			/* Enable port G clock */
+			RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;
 
-      /* Select output mode */
-      GPIOG->MODER |= GPIO_MODER_MODER13_0;
+			/* Select output mode */
+			GPIOG->MODER |= GPIO_MODER_MODER13_0;
 
-      /* Select output type push-pull */
-      GPIOG->OTYPER &= ~(GPIO_OTYPER_OT_13);
+			/* select output type push-pull */
+			GPIOG->OTYPER &= ~(GPIO_OTYPER_OT_13);
 
-      /* Select output speed very high */
-      GPIOG->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR13_0 | GPIO_OSPEEDER_OSPEEDR13_1);
+			/* Select output speed very high */
+			GPIOG->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR13_0 | GPIO_OSPEEDER_OSPEEDR13_1);
 
-      /* Enable SYSCFG clock */
-      RCC->APB2ENR |= RCC_APB2LPENR_SYSCFGLPEN;
+			/* Enable SYSCFG clock */
+			RCC->APB2ENR |= RCC_APB2LPENR_SYSCFGLPEN;
 
-      /* Enable compensation cell */
-      SYSCFG->CMPCR |= SYSCFG_CMPCR_CMP_PD;
+			/* Wait till compensation cell is ready */
+			//while((SYSCFG->CMPCR & SYSCFG_CMPCR_READY) != SYSCFG_CMPCR_READY);
 
-      /* Wait till compensation cell is ready */
-      while((SYSCFG->CMPCR & SYSCFG_CMPCR_READY) != SYSCFG_CMPCR_READY)
-      {
+			/* Select no pull */
+			GPIOG->PUPDR &= ~(GPIO_PUPDR_PUPDR13);
 
-      }
-
-      /* Select no pull */
-      GPIOG->PUPDR &= ~(GPIO_PUPDR_PUPDR13);
-
-      break;
-
-    default:
-      break;
-  }
+			break;
+		default:
+			break;
+	}
 }
 
-/**
- * @brief   Turns on-board LED on
- * @note    EVAL_GREEN_LED -> PG13
- * @param   Led_Type led
- * @retval
- */
+
 void GPIO_TurnON_LED(Led_Type led)
 {
-  switch (led)
-  {
-    case EVAL_GREEN_LED:
-      GPIOG->BSRRL = GPIO_BSRR_BS_13;
-      break;
-
-    case EVAL_GREEN_LED_BITBAND:
-      GREEN_LED_BITBAND = 1;
-      break;
-
-    default:
-      break;
-  }
+	switch (led)
+	{
+		case EVAL_GREEN_LED:
+			GPIOG->BSRRL = GPIO_BSRR_BS_13;
+			break;
+		case EVAL_GREEN_LED_BITBAND:
+			GREEN_LED_BITBAND = 1;
+			break;
+		default:
+			break;
+	}
 }
 
-/**
- * @brief   Turns on-board LED off
- * @note    EVAL_GREEN_LED -> PG13
- * @param   Led_Type led
- * @retval
- */
+
 void GPIO_TurnOFF_LED(Led_Type led)
 {
-  switch (led)
-  {
-    case EVAL_GREEN_LED:
-      GPIOG->BSRRH = GPIO_BSRR_BS_13;
-      break;
-
-    case EVAL_GREEN_LED_BITBAND:
-      GREEN_LED_BITBAND = 0;
-      break;
-
-    default:
-      break;
-  }
+	switch (led)
+	{
+		case EVAL_GREEN_LED:
+			GPIOG->BSRRH = GPIO_BSRR_BS_13;
+			break;
+		case EVAL_GREEN_LED_BITBAND:
+			GREEN_LED_BITBAND = 0;
+			break;
+		default:
+			break;
+	}
 }
-
-/**
- * @}
- */
