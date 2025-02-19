8e2d9070341e73e942ff0c753307445252add6a6
ahmadmorad39 <ahmadmorad39@gmail.com>
2025-02-19 21:17:29 +0200

    DMA example

---
+++ DMA/src/inc/exti.h
@@ -0,0 +1,17 @@
+/*
+ * exti.h
+ *
+ *  Created on: Feb 18, 2025
+ *      Author: ahmad
+ */
+
+#ifndef INC_EXTI_H_
+#define INC_EXTI_H_
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_it.h"
+#include "stm32f429i_discovery.h"
+
+void EXTI_Init_PB();
+
+#endif /* INC_EXTI_H_ */
---
+++ DMA/src/inc/memcpy.h
@@ -0,0 +1,26 @@
+/*
+ * memcpy.h
+ *
+ *  Created on: Feb 18, 2025
+ *      Author: ahmad
+ */
+
+#ifndef INC_MEMCPY_H_
+#define INC_MEMCPY_H_
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_it.h"
+#include "stm32f429i_discovery.h"
+
+/**
+ * @brief  On-board LEDs
+ */
+typedef enum
+{
+	RETURN_STATUS_OK,
+	RETURN_STATUS_NOT_OK
+} ReturnStatus_Type;
+
+ReturnStatus_Type memcpy32(const uint32_t *src, uint32_t *dst, const uint32_t size);
+
+#endif /* INC_MEMCPY_H_ */
